50c3742dab4d9dca7821a7cd23873daaec4c78d5
elsieC <elschan90@gmail.com>
2015-06-21 13:00:11 +0000

    JS for followers/following only. complete.

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,4 +1,18 @@
 $(document).ready(function() {
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+
+$('.followers-label').on('click',function() {
+    $(".followers-label").css('color','black');
+   $(".following-label").css('color','#ccc');
+   $(".followings-list").hide(0);
+   $(".followers-list").show(0)
+});
+
+$('.following-label').on('click',function() {
+    $(".following-label").css('color','black');
+   $(".followings-list").show(0);
+    $(".followers-label").css('color', '#ccc');
+     $(".followers-list").hide(0);
+});
+
 });
