d168eae5cc08a3e975bc82aad5de6fee3c207771
elsieC <elschan90@gmail.com>
2015-06-21 16:26:35 +0000

    js addon

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -8,7 +8,22 @@ $('.followers-label').on('click',function() {
    $(".followers-list").show(0)
 });
 
+
+$('.followers_tab').on('click',function() {
+    $(".followers-label").css('color','black');
+   $(".following-label").css('color','#ccc');
+   $(".followings-list").hide(0);
+   $(".followers-list").show(0)
+});
+
 $('.following-label').on('click',function() {
+    $(".following-label").css('color','black');
+   $(".followings-list").show(0);
+    $(".followers-label").css('color', '#ccc');
+     $(".followers-list").hide(0);
+});
+
+$('.following_tab').on('click',function() {
     $(".following-label").css('color','black');
    $(".followings-list").show(0);
     $(".followers-label").css('color', '#ccc');
